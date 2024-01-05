7cfb9f28bb9cdde69fa5b91739bb858e7ef76c1a
Grzegorz Krason <grzegorz.krason@gmail.com>
2024-01-06 00:56:09 +0100

    fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,8 @@ import subprocess
 from pathlib import Path
 
 def sh(cmd, cwd="tests/data"):
-    return subprocess.check_output(f'cd {cwd} && ' + cmd + ' 2>&1 | cat', stderr=subprocess.STDOUT, shell=True).decode().splitlines()
+    raw_out = subprocess.check_output(f'cd {cwd} && ' + cmd + ' 2>&1 | cat', stderr=subprocess.STDOUT, shell=True).decode()
+    return [line[2:] if line.startswith("$ ") else line for line in raw_out.splitlines()]
 
 def test_version():
     out = sh("""cada -V""")
