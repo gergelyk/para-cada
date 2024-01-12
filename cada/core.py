88d1a2ceb40e3e8ae7fa24973f47bc8f565bb9bb
Grzegorz Krason <grzegorz.krason@gmail.com>
2024-01-13 00:28:35 +0100

    dry mode prints to stdout

--- cada/core.py
+++ cada/core.py
@@ -154,7 +154,7 @@ class Runner:
 
     def _run_in_dry_mode(self, cmd):
         with reserved_printer as printer:
-            printer.show_blue(cmd)
+            print(cmd)
 
     def _run_in_shell(self, cmd):
         if not self._quiet and reserved_printer.is_tty:
